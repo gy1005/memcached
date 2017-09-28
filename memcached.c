239b13505f848ecabaf906a03783faee1a3b5767
Yu Gan <yg397@cornell.edu>
2017-09-28 16:19:56 -0400

    fixed bugs

--- memcached.c
+++ memcached.c
@@ -4882,7 +4882,7 @@ static void drive_machine(conn *c) {
                 break;
             case READ_DATA_RECEIVED:
 
-                struct timeval tv;s                		
+                struct timeval tv;               		
                 gettimeofday(&tv, NULL);
                 pthread_mutex_lock(&screen_lock);
                 printf("start %llu\n", (unsigned long long) (tv.tv_sec * 1000000 + tv.tv_usec));
